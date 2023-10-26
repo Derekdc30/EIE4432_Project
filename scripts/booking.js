1fa0ab9b239de40b7447f3ad0f69f0fec978ef55
Derekdc30 <derekchoy30@gmail.com>
2023-10-26 17:27:44 +0800

    Update booking.js

--- scripts/booking.js
+++ scripts/booking.js
@@ -1,9 +1,12 @@
 $(document).ready(function () {
+    displayseat();
+});
+function displayseat(){
     var svgCircle = document.getElementById("svg");
     let y= 50;
     let x = 50; 
     var NS = "http://www.w3.org/2000/svg";
-    for(let i =1; i<=40; i++)
+    for(let i =1; i<=80; i++)
     {
         var rect = document.createElementNS(NS, "rect");
         console.log(rect);
@@ -14,11 +17,18 @@ $(document).ready(function () {
         rect.setAttribute("stroke", "green");
         rect.setAttribute("strokeWidth", 4);
         rect.setAttribute("fill", "yellow");
+        rect.setAttribute("id",i);
+        rect.addEventListener("click", handleClick);
         svgCircle.appendChild(rect);
         x+=50;
         if(i%10==0){
-            y= y+50;
+            y+=50;
             x=50;
         }
     }
-});
+}
+
+function handleClick(event) {
+    var rectID = event.target.id; // Retrieve the ID of the clicked rect
+    alert("Clicked rect ID:", rectID);
+  }
