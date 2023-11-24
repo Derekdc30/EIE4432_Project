65226dfb78d4c2f7f0eaa70f90aa7f9359bc3135
Derekdc30 <derekchoy30@gmail.com>
2023-11-24 23:17:30 +0800

    11/24

--- src/route.js
+++ src/route.js
@@ -30,11 +30,7 @@ route.post('/login',form.none(), async (req, res)=>{
       req.session.logged = true;
       req.session.username = user.username;
       req.session.role = user.role;
-      req.session.timestamp = Date.now();
-      const rememberMeToken = generateToken(); 
-      if (rememberMe) {
-        res.cookie('remember_me', rememberMeToken, { httpOnly: true, maxAge: 30 * 24 * 60 * 60 * 1000 }); // 30 days
-      } 
+      req.session.timestamp = Date.now(); 
       res.json({
         status: 'success',
         user: {
@@ -195,14 +191,4 @@ route.post('/newevents', form.none(), async (req, res) => {
 
 });
 
-const generateToken = () => {
-  const tokenLength = 32;
-  const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-  let token = '';
-  for (let i = 0; i < tokenLength; i++) {
-    const randomIndex = Math.floor(Math.random() * characters.length);
-    token += characters.charAt(randomIndex);
-  }
-  return token;
-};
 export default route;
