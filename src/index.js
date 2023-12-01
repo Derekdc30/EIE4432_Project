772c9f99fd7cb08565bc9b436fb68060eae760f3
Derekdc30 <derekchoy30@gmail.com>
2023-12-01 20:37:59 +0800

    12/1

--- src/index.js
+++ src/index.js
@@ -28,7 +28,20 @@ app.use(express.static('static'));
 app.use(express.static('assets'));
 app.use('/', express.static(path.join(process.cwd(), '/static')));
 
-
+const PREAUTH_KEY = '<EIE4432>';
+app.use((req, res, next) => {
+    if (!req.session?.allow_access) {
+        if (req.query?.authkey === PREAUTH_KEY) {
+            req.session.allow_access = true;
+        } else {
+            res.status(401).json({
+                status: 'failed',
+                message: 'Unauthorized'
+            });
+        }
+    }
+    next();
+});
 
 app.listen(8080, () => {
   console.log('Server is running on port 8080');
